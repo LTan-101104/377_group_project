eafb375c9fa990160e3a67320ec6bd74ff40c417
Tan Le <letan101104@gmail.com>
2025-05-02 16:05:19 -0400

    Add 2 tests for read work_load

--- test/test.cpp
+++ test/test.cpp
@@ -11,6 +11,38 @@ TEST(SchedulingTest, ReadWorkload1) {
   EXPECT_EQ(pq.size(), 3);
 }
 
+//test for reading new workload 
+//read new worload1
+TEST (SchedulingTest, ReadWorkloadNew){
+  pqueue_arrival pq = read_workload("workloads/new_workload_01.txt");
+  EXPECT_EQ(pq.size(), 3);
+  while (!pq.empty()){
+    Process p = pq.top();
+    EXPECT_EQ(p.arrival, 0);
+    EXPECT_EQ(p.duration, 10);
+    EXPECT_EQ(p.time_demand, 10);
+    pq.pop();
+  }
+}
+
+//test for reading new workload2
+TEST (SchedulingTest, ReadWorkloadNew2){
+  pqueue_arrival pq = read_workload("workloads/new_workload_02.txt");
+  EXPECT_EQ(pq.size(), 3);
+  Process p1 = pq.top();
+  EXPECT_EQ(p1.arrival, 0);
+  EXPECT_EQ(p1.duration, 100);
+  EXPECT_EQ(p1.time_demand, 10);
+  pq.pop();
+  while (!pq.empty()){
+    Process p = pq.top();
+    EXPECT_EQ(p.arrival, 0);
+    EXPECT_EQ(p.duration, 10);
+    EXPECT_EQ(p.time_demand, 10);
+    pq.pop();
+  }
+}
+
 TEST(SchedulingTest, FIFO1) {
   pqueue_arrival pq = read_workload("workloads/workload_01.txt");
   list<Process> xs = fifo(pq);
@@ -26,7 +58,32 @@ TEST(SchedulingTest, MLFQ_test_simple_queue_1){
   int NUM_Q = 1;
   int time_reboost = 10000; // high time reboost to avoid reboosting
   int time_slice = 10;
-  list<Process> xs = read_workload("workloads/new_workload_01.txt");
+  pqueue_arrival pq = read_workload("workloads/new_workload_01.txt")
+  list<Process> xs = MLFQ(pq, time_reboost, NUM_Q, time_slice);
+  float t = avg_turnaround(xs);
+  float r = avg_response(xs);
+  EXPECT_FLOAT_EQ(t, 20);
+  EXPECT_FLOAT_EQ(r, 10);
+}
+
+TEST(SchedulingTest, MLFQ_test_simple_queue_2){
+  //simple case, uniform time demand, no reboost, only 1 num queue
+  int NUM_Q = 1;
+  int time_reboost = 10000; // high time reboost to avoid reboosting
+  int time_slice = 10;
+  list<Process> xs = read_workload("workloads/new_workload_02.txt");
+  float t = avg_turnaround(xs);
+  float r = avg_response(xs);
+  EXPECT_FLOAT_EQ(t, 20);
+  EXPECT_FLOAT_EQ(r, 10);
+}
+
+TEST(SchedulingTest, MLFQ_test_double_queue_1){
+  //simple case, uniform time demand, no reboost, only 1 num queue
+  int NUM_Q = 1;
+  int time_reboost = 10000; // high time reboost to avoid reboosting
+  int time_slice = 10;
+  list<Process> xs = read_workload("workloads/new_workload_02.txt");
   float t = avg_turnaround(xs);
   float r = avg_response(xs);
   EXPECT_FLOAT_EQ(t, 20);
