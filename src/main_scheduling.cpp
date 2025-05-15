72206f300514bca08387b117c61fdd09902cf922
Tan Le <letan101104@gmail.com>
2025-05-15 15:59:58 -0400

    Adjust main_scheduling

--- src/main_scheduling.cpp
+++ src/main_scheduling.cpp
@@ -3,20 +3,20 @@
 #include <string>
 
 using namespace std;
-void compare_MLFQ(string filename, int time_reboost, int num_queues, int time_slice){
-  pqueue_arrival workload = read_workload(filename);
+void compare_MLFQ(pqueue_arrival workload, int time_reboost, int num_queues, int time_slice){
   list<Process> mlfq_complete = MLFQ(workload, time_reboost, num_queues, time_slice);
   cout << "**MLFQ - time slice: " << time_slice << ", time reboost: " << time_reboost << ", num_queues: " << num_queues<< "- :" << endl << "- Average turnaround time: " << avg_turnaround(mlfq_complete) << endl << "- Average response time: " << avg_response(mlfq_complete) << endl;
 }
 
-void compare_STCF_RR(string filename, int time_slice){
-  pqueue_arrival workload = read_workload(filename);
-  list<Process> stcf_complete = stcf(workload);
+void compare_RR(pqueue_arrival workload, int time_slice){
   list<Process> rr_complete = rr(workload, time_slice);
-  cout << "**STCF: " << endl << "- Average turnaround time: " << avg_turnaround(stcf_complete) << endl << "- Average response time: " << avg_response(stcf_complete) << endl;
   cout << "**RR: - time slice: " << time_slice << "- :" << endl << "- Average turnaround time: " << avg_turnaround(rr_complete) << endl << "- Average response time: " << avg_response(rr_complete) << endl;
 }
 
+void compare_STCF(pqueue_arrival workload){
+  list<Process> stcf_complete = stcf(workload);
+  cout << "**STCF:" << endl << "- Average turnaround time: " << avg_turnaround(stcf_complete) << endl << "- Average response time: " << avg_response(stcf_complete) << endl;
+}
 
 
 int main(int argc, char* argv[]) {
@@ -53,14 +53,16 @@ int main(int argc, char* argv[]) {
   int time_reboost[] = {20, 100};
   int num_queues[] = {1,4};
   int time_slice[] = {5, 10};
-  char* filename[] = {"main_workload/workload_01.txt", "main_workload/workload_02.txt", "main_workload/workload_03.txt", "main_workload/workload_04.txt", "main_workload/workload_05.txt"};
-  for (char* filename : filename){
+  const char* filename[] = {"main_workload/workload_01.txt", "main_workload/workload_02.txt", "main_workload/workload_03.txt", "main_workload/workload_04.txt", "main_workload/workload_05.txt"};
+  for (const char* filename : filename){
     cout << "___________________________________Running on " << filename << "___________________________________" << endl;
+    pqueue_arrival workload = read_workload(filename);
+    compare_STCF(workload);
     for (int time_slice : time_slice){
-      compare_STCF_RR(filename, time_slice);
+      compare_RR(workload, time_slice);
       for (int num_queues : num_queues){
         for (int time_reboost : time_reboost){
-          compare_MLFQ(filename, time_reboost, num_queues, time_slice);
+          compare_MLFQ(workload, time_reboost, num_queues, time_slice);
         }
       }
     }
